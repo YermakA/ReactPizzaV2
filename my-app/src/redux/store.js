25c6dceee9f4d9be1305cb8782409c1ad30b2ba2
YermakA <ermak.080@mail.ru>
2023-08-24 14:26:21 +0800

    cartSlice created

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from './slices/searchSlice'
 import sortReducer from './slices/sortSlice'
 import categoryReducer from './slices/categorySlice'
+import cartReducer from './slices/cartSlice'
 const store = configureStore(
   {
     reducer: {
       search: searchReducer,
       sort: sortReducer,
-      category: categoryReducer
+      category: categoryReducer,
+      cart: cartReducer
     }
   }
 )
